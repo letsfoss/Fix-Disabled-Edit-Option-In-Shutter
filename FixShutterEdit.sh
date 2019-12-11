20eb55dbff0677a84ae90a691e928fa1e434f2bf
Let's FOSS <49740790+letsfoss@users.noreply.github.com>
2019-12-11 13:40:08 +0530

    Update FixShutterEdit.sh

--- FixShutterEdit.sh
+++ FixShutterEdit.sh
@@ -7,7 +7,7 @@
 
 # Install Flux
 FixShutterEdit (){
-    printf "\n\U2692  Fixing Shutter Edit Button\n\n\U1F511 You just need to type your Password one time. If you're not running as root\n\n\U1F197 This script will download and install additional packages.\n\n\U1F6E1  Which is completly safe\n\n"
+    printf "\n\U2692  Fixing Shutter Edit Button\n\n\U1F511 You just need to type your Password one time. If you're not running as root\n\n\U1F197 This script will download and install additional packages.\n\n\U1F6E1  Which is completly safe\n\n\U1F622 Don't worry about broken packages. Everything will be fixed automatically\n\n\U231A  Time Consuming Process. Thanks for your patience"
     sleep 5
     printf "\n\U1F680 Starting Installation\n\n"
     sleep 3
@@ -29,7 +29,7 @@ FixShutterEdit (){
     sudo rm -rf libgoocanvas3_1.0.0-1_amd64.deb
     sudo rm -rf libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
     sleep 3
-    printf "\nRunning autoremove"
+    printf "\nAutoremove to Fix Broken Packages"
     sudo apt autoremove -y
     sleep 3
     printf "\n\n\U1F984 Edit button fixed\n\n\U1F9D0 Open and Check Shutter. Bye Bye\n\n\n\U1F449 Visit https://letsfoss.com for more linux tutorials\n\n\n"
